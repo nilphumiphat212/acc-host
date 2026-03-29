56b2f7234d3fbfaf0b551fa12cec7ce00e208236
zphumiphatj <zphumiphatj@pttep.com>
2026-03-29 18:35:33 +0700

    [Nil] Update files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4042695538" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "624640136" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
